692d35a3d37d00da2a6d0834716fb181a1921475
Tenkorang Daniel <kwamekyeimonies@gmail.com>
2023-02-27 15:48:45 +0000

    max-heaps

--- heaps/heaps.go
+++ heaps/heaps.go
@@ -1,2 +1,32 @@
 package heaps
 
+type MaxHeap struct{
+	array []int
+}
+
+//Insert adds an element to the heap
+func (h *MaxHeap) Insert(key int){
+	h.array = append(h.array, key)
+	
+}
+
+func (h *MaxHeap) maxHeafifyup(index int){
+
+}
+
+func Parent(i int)int{
+
+}
+
+func Left(i int)int{
+
+}
+
+func Right(i int) int{
+
+}
+
+
+func (h *MaxHeap) swap(i1,i2 int){
+	h.array[i1],h.array[i2] = h.array[i2], h.array[i1]
+}
